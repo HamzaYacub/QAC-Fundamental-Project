b83fafbcfe7776e84536b849f3ff0fb91df75c14
Hamza Yacub <hamzayacub97@gmail.com>
2020-05-13 18:44:01 +0000

    added html pages

--- application/__init__.py
+++ application/__init__.py
@@ -2,14 +2,11 @@ from flask import Flask
 from os import getenv
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
-from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']=getenv('CAR_HIRE_DB_URI')
 app.config['SECRET_KEY']=getenv('SECRET_KEY')
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
-login_manager.login_view = 'login'
 
 from application import routes
--- application/routes.py
+++ application/routes.py
@@ -4,5 +4,24 @@ from application import app
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html', title="Home page")
+    return render_template('home.html', title="Home")
 
+@app.route('/rent')
+def rent():
+    return render_template('rent.html', title="Rent a Car")
+
+@app.route('/add_car')
+def add_car():
+    return render_template('addCar.html', title="Add a Car")
+
+@app.route('/rental_history')
+def rental_history():
+    return render_template('rentalHistory.html', title="Rental History")
+
+@app.route('/login')
+def login():
+    return render_template('login.html', title="Login")
+
+@app.route('/register')
+def register():
+    return render_template('register.html', title="Register")
