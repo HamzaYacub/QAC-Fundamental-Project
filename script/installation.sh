b997a16b3767368b1989c1f8b92fd8cd3282cfd2
Hamza Yacub <hamzayacub97@gmail.com>
2020-05-20 13:26:59 +0000

    changes to installation.sh

--- script/installation.sh
+++ script/installation.sh
@@ -10,8 +10,8 @@ sudo apt install python3-venv -y
 
 python3 -m venv venv
 
-. venv/bin/activate
+source /var/lib/jenkins/workspace/carhire_freestyle/venv/bin/activate
 
 pip3 install -r requirements.txt
 
-gunicorn --bind=0.0.0.0:5000 application:app
+gunicorn --bind=0.0.0.0:5000 application:/var/lib/jenkins/workspace/carhire_freestyle/app
