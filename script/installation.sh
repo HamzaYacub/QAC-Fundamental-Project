73e645c67d0f7e4106bb1bc3b7b5eb690c3108fd
Hamza Yacub <hamzayacub97@gmail.com>
2020-05-20 13:51:28 +0000

    changes to installation.sh

--- script/installation.sh
+++ script/installation.sh
@@ -16,4 +16,10 @@ pip3 install -r requirements.txt
 
 cd /var/lib/jenkins/workspace/carhire_freestyle
 
+export CAR_HIRE_DB_URI=mysql+pymysql://root:helloworld@34.89.27.192/carhire_db
+
+export SECRET_KEY=kjg345g34kjg53jk4g5kj34h535n
+
+export CSRF_SECRET_KEY=hjkg5324gg890ddy8g98dygyds
+
 gunicorn --bind=0.0.0.0:5000 app:app
