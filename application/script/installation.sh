b443582c05a1e314580f0f9ed4df06d0ccbd0612
Hamza Yacub <hamzayacub97@gmail.com>
2020-05-20 12:15:45 +0000

    added system d services

---
+++ application/script/installation.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+sudo apt update -y
+
+sudo apt install python3 -y
+
+sudo apt install python3-pip -y
+
+sudo apt install python3-venv -y
+
+python3 -m venv venv
+
+. venv/bin/activate
+
+pip3 install -r requirements.txt
+
+gunicorn --bind=0.0.0.0:5000 application:app
